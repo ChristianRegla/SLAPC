f7c7c5567172112ac2446255be242603f89a4cce
Bruci <bruceallendenis@gmail.com>
2024-11-30 15:07:32 -0600

    Detalles de los componentes
    Ya quedo la funcionalidad de detalles de los componentes, en vez de un fragment se hizo con una activity el diseño se trato de ser como el de Lucid

--- app/src/main/java/com/example/slapc/ui/catalogo/ProductoAdapter.kt
+++ app/src/main/java/com/example/slapc/ui/catalogo/ProductoAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.slapc.ui.catalogo
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.AsyncTask
@@ -10,6 +11,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.slapc.R
+import com.example.slapc.ui.detalles.ProductoDetailActivity
 import java.net.HttpURLConnection
 import java.net.URL
 
@@ -29,16 +31,23 @@ class ProductoAdapter(private val productos: List<Producto>) : RecyclerView.Adap
         val producto = productos[position]
 
         if (producto.reflimagen is Int) {
-            // Si es un recurso local (Int)
             holder.imgProducto.setImageResource(producto.reflimagen)
-        } else if (producto.reflimagen is String) {
-            // Si es una URL (String)
-            holder.imgProducto.setImageResource(producto.reflimagen as Int)
-
         }
-
         holder.tvNombre.text = producto.nombre
         holder.tvDescription.text = producto.detallesTecnicos
+
+        // Click
+        holder.itemView.setOnClickListener {
+            val context = holder.itemView.context
+            val intent = Intent(context, ProductoDetailActivity::class.java)
+            intent.putExtra("id", producto.id)
+            intent.putExtra("nombre", producto.nombre)
+            intent.putExtra("reflimagen", producto.reflimagen.toString())
+            intent.putExtra("precio", producto.precio)
+            intent.putExtra("categoria", producto.categoria)
+            intent.putExtra("detallesTecnicos", producto.detallesTecnicos)
+            context.startActivity(intent)
+        }
     }
 
     private class LoadImageTask(val imageView: ImageView) : AsyncTask<String, Void, Bitmap?>() {
