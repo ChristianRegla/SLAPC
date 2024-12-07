229ded6cb81b3f5b083d69e17d684b9605fcc8bc
ChristianRegla <119276883+ChristianRegla@users.noreply.github.com>
2024-12-06 21:53:01 -0600

    Diseño a carrito

--- app/src/main/java/com/example/slapc/RepositorioComponentes.kt
+++ app/src/main/java/com/example/slapc/RepositorioComponentes.kt
@@ -3,6 +3,19 @@ package com.example.slapc
 object RepositorioComponentes {
     private val catalogoComponentes = mutableListOf<Componente>()
 
+    // Productos para calar
+    init {
+        catalogoComponentes.addAll(
+            listOf(
+                Componente("Procesador Intel i7", "intel_i7", 320.0, CategoriaComponente.CPU, "8 núcleos, 3.6GHz"),
+                Componente("SSD Samsung 1TB", "ssd_samsung", 120.0, CategoriaComponente.ALMACENAMIENTO, "1TB NVMe"),
+                Componente("Monitor LG 24''", "monitor", 200.0, CategoriaComponente.MONITOR, "Full HD, IPS"),
+                Componente("Mouse Logitech", "mouse_logitech", 1000.0, CategoriaComponente.MOUSE, "Óptico, ergonómico"),
+                Componente("Teclado Mecánico", "teclado_mecanico", 1200.0, CategoriaComponente.TECLADO, "RGB, switch azul")
+            )
+        )
+    }
+
     fun agregarComponente(comp: Componente) {
         catalogoComponentes.add(comp)
     }
