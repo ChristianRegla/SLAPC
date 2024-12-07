47383af679fbb91e437f0e969ebe3017135bbc00
ChristianRegla <119276883+ChristianRegla@users.noreply.github.com>
2024-12-06 23:49:28 -0600

    Por si la kgo pt.2

--- app/src/main/java/com/example/slapc/RepositorioComponentes.kt
+++ app/src/main/java/com/example/slapc/RepositorioComponentes.kt
@@ -7,11 +7,11 @@ object RepositorioComponentes {
     init {
         catalogoComponentes.addAll(
             listOf(
-                Componente("Procesador Intel i7", "intel_i7", 320.0, CategoriaComponente.CPU, "8 núcleos, 3.6GHz"),
-                Componente("SSD Samsung 1TB", "ssd_samsung", 120.0, CategoriaComponente.ALMACENAMIENTO, "1TB NVMe"),
-                Componente("Monitor LG 24''", "monitor", 200.0, CategoriaComponente.MONITOR, "Full HD, IPS"),
-                Componente("Mouse Logitech", "mouse_logitech", 1000.0, CategoriaComponente.MOUSE, "Óptico, ergonómico"),
-                Componente("Teclado Mecánico", "teclado_mecanico", 1200.0, CategoriaComponente.TECLADO, "RGB, switch azul")
+                Componente("Intel Core i7-14700", "intel_i7", 7599.00, CategoriaComponente.CPU, "Procesador Intel Core i7-14700 / LGA1700 / 2.10GHz, 20-Core / 33MB Smart Cache / 14va. Generación / Intel UHD GRaphics 770 / Raptor Lake / BX8071514700"),
+                Componente("Unidad de Estado Sólido M.2 Samsung 980 / 1TB", "ssd_samsung", 2499.00, CategoriaComponente.ALMACENAMIENTO, "Unidad de Estado Sólido M.2 Samsung 980 / 1TB / NVMe Interfaz de Estado Sólido con Tecnología V-NAND / MZ-V8V1T0B/AM"),
+                Componente("Monitor Gamer LG UltraGear 24 FHD / 24GL600F-B", "monitor", 4899.00, CategoriaComponente.MONITOR, "Monitor Gamer LG UltraGear 24\" FHD / 1ms - 144Hz / HDMI x2 / 24GL600F-B"),
+                Componente("Mouse Gamer Logitech G502 Lightspeed Wireless", "mouse_logitech", 1999.00, CategoriaComponente.MOUSE, "Mouse Gamer Logitech G502 Lightspeed Wireless / 16000 DPI / 910-005566"),
+                Componente("Logitech Teclado Gamer PRO X TKL", "teclado_mecanico", 3199.00, CategoriaComponente.TECLADO, "Logitech Teclado Gamer PRO X TKL / Mecánico / Switch GX Brown / Inalámbrico / Negro / (Inglés) / 920-012127")
             )
         )
     }
