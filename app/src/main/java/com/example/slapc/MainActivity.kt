18ef722170e73956d463a810700c450d5c693300
ChristianRegla <119276883+ChristianRegla@users.noreply.github.com>
2024-12-07 19:53:44 -0600

    Idea de un about us
    Diseño inicial, pensaba agregar más cosas pero quiero proponer ciertos detalles

--- app/src/main/java/com/example/slapc/MainActivity.kt
+++ app/src/main/java/com/example/slapc/MainActivity.kt
@@ -42,7 +42,8 @@ class MainActivity : AppCompatActivity() {
                 R.id.nav_login,
                 R.id.nav_catalogo,
                 R.id.nav_carrito,
-                R.id.nav_pedido
+                R.id.nav_pedido,
+                R.id.nav_about_us
             ),
             drawerLayout
         )
